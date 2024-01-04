9e73052c0d2ca3711d2b5e12828fac1068c22a00
ginchandesu <142890093+ginchandesu@users.noreply.github.com>
2024-01-04 11:03:49 +0530

    update

--- operation.py
+++ operation.py
@@ -1,2 +1,8 @@
 import ADD.addition
 print(ADD.addition.add(100,200))
+print("another way to call package")
+from SUB.substraction import substract
+print(substract(300,10))
+print("---------subpackage------------")
+from ADD.advance.multiplication import mul
+print(mul(10,30))
